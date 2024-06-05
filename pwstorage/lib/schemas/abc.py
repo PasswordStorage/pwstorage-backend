7dae2add6f2cc74f7cb4e46003a295619e5ed157
D0Nater <dnaterfilin12@gmail.com>
2024-06-05 09:10:53 +0300

    Created camel case for API

--- pwstorage/lib/schemas/abc.py
+++ pwstorage/lib/schemas/abc.py
@@ -6,10 +6,16 @@ from typing import Any, Generator
 from pydantic import BaseModel, ConfigDict
 
 
+def to_camel(string: str) -> str:
+    """Format string to camelCase."""
+    parts = string.split("_")
+    return parts[0] + "".join(word.capitalize() for word in parts[1:])
+
+
 class BaseSchema(BaseModel, ABC):
     """Base schema."""
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, alias_generator=to_camel, populate_by_name=True)
 
     def iterate_set_fields(self, exclude: list[str] = []) -> Generator[tuple[str, Any], None, None]:
         """Iterate over set fields."""
