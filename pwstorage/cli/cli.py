c78b9b8d1c7820d16127486be1011511d0395dd3
D0Nater <dnaterfilin12@gmail.com>
2024-07-10 20:28:34 +0300

    Fixed

--- pwstorage/cli/cli.py
+++ pwstorage/cli/cli.py
@@ -7,5 +7,5 @@ from pwstorage.lib.utils.log import configure_logging
 
 @click.group()
 def cli() -> None:
-    """Control interface for VMM API."""
+    """Control interface for pwstorage API."""
     configure_logging()
